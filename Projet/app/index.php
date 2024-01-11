a2944fae994464bda176a1e2c179ed060c25f88c
November304 <nathan.p3pro@gmail.com>
2024-01-11 10:38:49 +0100

    Changement -> Ca marche pas
    Mais normal car eduroam donc faut test sans

--- Projet/app/index.php
+++ Projet/app/index.php
@@ -1,31 +1,27 @@
 <?php
 
+require('vendor/autoload.php');
+
+use \PhpMqtt\Client\MqttClient;
+
 // phpinfo();
 echo "<a href='Page2.php'>Page 2</a>";
-use PhpMqtt\Client\MqttClient;
-
-// Broker
-
-$broker = new MqttClient('tcp://localhost:1883', 'BrokerClient');
-$broker->connect();
-
-$message = 'Hello from the Broker!';
-$broker->publish('topic/broker', $message, 0, true);
-
-$broker->disconnect();
 
 // Listener
 
-$listener = new MqttClient('tcp://localhost:1883', 'ListenerClient');
-$listener->connect();
+$server   = 'lab.iut-blagnac.fr';
+$port     = 1883;
 
-$listener->subscribe('topic/broker', function ($topic, $message) {
-    echo "Received message on topic '$topic': $message\n";
-});
+$mqtt = new MQTTClient($server, $port);
+$mqtt->connect();
+
+$mqtt->subscribe('#', function ($topic, $message, $retained, $matchedWildcards) {
+    echo sprintf("Received message on topic [%s]: %s\n", $topic, $message);
+}, 0);
 
 // Attendez un certain temps pour que le listener puisse recevoir le message
-sleep(5);
+$mqtt->loop(true);
 
-$listener->disconnect();
+$mqtt->disconnect();
 
 ?>
