36b59be06223a16f67e8aa8038481a9638305f6d
November304 <nathan.p3pro@gmail.com>
2024-01-12 08:04:49 +0100

    Creation du docker compose
    Amélioration du docker pour qu'il ait une base de données aussi

--- Projet/create.bat
+++ Projet/create.bat
@@ -1,5 +1,5 @@
 REM Build de l'image Docker
-docker build -t sae .
+docker-compose build
 
 REM Exécution du conteneur Docker
-docker run -v C:\Users\Fontanilles\OneDrive\Documents\GitHub\SAE-ALT-S3-Dev-23-24-LocURa-Equipe-4\Projet\app:/app -it -p 8080:80 sae
+docker-compose up
