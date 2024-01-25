33fc992e54a1ba81fe7151f05b7c14046b884592
AlexiFon <alexi.fonta@outlook.com>
2024-01-25 20:03:57 +0100

    Ajout du scriptCreerCercles.js

--- Projet/app/scriptRecupererDonneesRanging.js
+++ Projet/app/scriptRecupererDonneesRanging.js
@@ -1,4 +1,6 @@
-import {GestionDonneesLignes} from "./scriptCreerLignes.js";
+import { GestionDonneesCercles } from "./scriptCreerCercles.js";
+import { GestionDonneesLignes } from "./scriptCreerLignes.js";
+
 document.addEventListener("DOMContentLoaded", function() {
     $.ajax({
         url: 'donnes.php',
@@ -7,11 +9,13 @@ document.addEventListener("DOMContentLoaded", function() {
         data:{request: "rangingData"},
         success: function (data) {
             console.log('Données récupérées avec succès :', data);
+            GestionDonneesCercles(data);
             GestionDonneesLignes(data);
-            
         },
         error: function(error) {
             console.error('Erreur lors de la récupération des données de ranging :', error);
         }
     });
 });
+
+
