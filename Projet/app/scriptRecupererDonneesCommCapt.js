8782c090fecc08931d84625869d6cae1243d86ce
Aymen Charouki <charoukiaymen@gmail.com>
2024-01-28 12:41:58 +0100

    ajax recup donnees comm

--- Projet/app/scriptRecupererDonneesCommCapt.js
+++ Projet/app/scriptRecupererDonneesCommCapt.js
@@ -1,15 +1,28 @@
-document.addEventListener('DOMContentLoaded', function() {
-    $ajax({
-        url : 'donnes.php',
-        method : 'post',
-        dataType : 'json',
-        data:{request:"getCommCapt"},
-        success : function(data){
-            console.log('Données récupérées avec succès');
+document.addEventListener("DOMContentLoaded", function () {
+    // Definition de la fonction pour recuperer et traiter les données
+    function fetchData() {
+        $.ajax({
+            url: 'donnes.php',
+            method: 'post',
+            dataType: 'json',
+            data: { request: "ClignoterPoints" },
+            success: function (data) {
+                console.log('Données récupérées avec succès :', data[0]);
+                var point = document.getElementById(data[0].idCapteur)
+                if(point==null){
+                    console.log(" Point en comm : " + data[0].idCapteur)
+                    createPoints(data)
+                } 
+            },
+            error: function (error) {
+                console.error(error);
+            }
+        });
+    }
 
-        },
-        error: function(error){
-            console.error('Données non récupérées',error);
-        }
-    });
-}); 
+    // Appel initial de la fonction 
+    fetchData();
+
+    // Mettre un intervale pour appeler la fonction toutes les 2 secondes (2000 millisecondes)
+    setInterval(fetchData, 2000);
+});
