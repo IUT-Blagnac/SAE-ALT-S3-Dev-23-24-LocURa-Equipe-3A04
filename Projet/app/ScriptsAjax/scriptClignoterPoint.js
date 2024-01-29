ced7bf95465ff44c4ebe476cbb5ad0048e5860e9
Aymen Charouki <charoukiaymen@gmail.com>
2024-01-29 22:25:57 +0100

    ClignoterPoints

--- Projet/app/ScriptsAjax/scriptClignoterPoint.js
+++ Projet/app/ScriptsAjax/scriptClignoterPoint.js
@@ -1,4 +1,3 @@
-import { INTERVALLE_MAJ_COMM_CAPT,CLIGNOTEMENT_DUREE,CLIGNOTEMENT_INTERVALLE } from "../DiversJavaScripts/constantes";
 document.addEventListener("DOMContentLoaded", function () {
     // Definition de la fonction pour recuperer et traiter les données
     function fetchData() {
@@ -8,25 +7,16 @@ document.addEventListener("DOMContentLoaded", function () {
             dataType: 'json',
             data: { request: "ClignoterPoints" },
             success: function (data) {
-                console.log('Données récupérées avec succès :', data);
 
-                console.log(" Point en comm : " + data[0].idCapteur)
-                createPoints(data)
                 for (var i = 0; i < data.length; i++) {
                     console.log(" Point en comm : " + data[i].idCapteur)
                     var point = document.getElementById(data[i].idCapteur);
-                    point.style.opacity = 0.25;
+                        point.style.opacity = 0.25;
 
                     // Clignoter le point
-                    var interval = setInterval(function () {
                         point.style.opacity = (point.style.opacity == 0.25) ? 1 : 0.25;
-                    }, CLIGNOTEMENT_INTERVALLE); // Clignoter toutes les cliognotementIntervalles millisecondes
 
-                    // Arret de clignotement après l'intervalle de temps défini
-                    setTimeout(function () {
-                        clearInterval(interval);
                         point.style.opacity = 1;
-                    },CLIGNOTEMENT_DUREE); 
                 }
             },
             error: function (error) {
@@ -38,6 +28,6 @@ document.addEventListener("DOMContentLoaded", function () {
     // Appel initial de la fonction 
     fetchData();
 
-    // Mettre un intervale pour appeler la fonction toutes les 2 secondes (2000 millisecondes)
-    setInterval(fetchData, INTERVALLE_MAJ_COMM_CAPT);
+    // Mettre un intervale pour appeler la fonction toutes les 500 secondes (500 millisecondes)
+    setInterval(fetchData, 500);
 });
