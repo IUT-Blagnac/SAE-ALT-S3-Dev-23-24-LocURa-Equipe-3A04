41fdbabd42a8ff917ee2fde4c3f236a1c753f019
AlexiFon <alexi.fonta@outlook.com>
2024-01-29 22:41:13 +0100

    Update ajout couleur des cercles et update cahier de test

--- Projet/app/ScriptsCreationElements/scriptCreerCercles.js
+++ Projet/app/ScriptsCreationElements/scriptCreerCercles.js
@@ -18,6 +18,7 @@ function DessinerCercle($donnees) {
 
     let centreCercle = document.getElementById($donnees['initiator']);
     let rayon = $donnees['range'] * COEFF_TT; // Ajustez selon votre besoin
+    let couleur = centreCercle.style.backgroundColor;
 
     // Création du cercle
     let cercle = document.createElement("div");
@@ -26,6 +27,7 @@ function DessinerCercle($donnees) {
     cercle.style.left = (parseFloat(centreCercle.style.left) - rayon + 5) + "px";
     cercle.style.top = (parseFloat(centreCercle.style.top) - rayon + 5) + "px";
     cercle.style.width = cercle.style.height = 2 * rayon + "px";
+    cercle.style.backgroundColor = couleur;
     
 
     document.getElementById("map").appendChild(cercle);
